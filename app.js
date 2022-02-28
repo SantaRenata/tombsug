b23c92f586c4894b1aca9928f98e16e1ee11817e
SantaRenata <renatasanta2002@gmail.com>
2022-02-28 08:26:19 +0100

    Kezdes

---
+++ app.js
@@ -0,0 +1,9 @@
+/*
+* File: app.js
+* Author: Sánta Renáta Diána
+* Copyright: 2022, Sánta Renáta Diána
+* Group: Szoft I N
+* Date: 2022-02-28
+* Github: https://github.com/SantaRenata/
+* Licenc: GNU GPL
+*/
