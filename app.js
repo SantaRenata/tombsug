2c13943c6cd14c99fd31b2539a34189a6bcb5cbf
SantaRenata <renatasanta2002@gmail.com>
2022-02-28 08:58:11 +0100

    Javascript megcsinalva

--- app.js
+++ app.js
@@ -8,3 +8,15 @@
 * Licenc: GNU GPL
 */
 
+var oldalIn = document.getElementById("oldal");
+var alfaIn = document.getElementById("oldal");
+var szamitGomb = document.getElementById("szamitGomb");
+var eredmenyIn = document.getElementById("eredmeny");
+
+szamitGomb.addEventListener("click", function(){
+    let oldal = oldalIn.value
+    let alfa = alfaIn.value
+    let sugar = 1/2*oldal*Math.sin(alfa);
+    console.log(sugar.toLocaleString());
+    eredmenyIn.value = (sugar.toLocaleString());
+});
